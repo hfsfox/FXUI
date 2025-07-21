96c047cefe528cc78e43894ea326982129133458
hfsfox <darkkitsunezx128k2309@gmail.com>
2025-07-21 09:53:52 +0000

    FXUI: widgets: menubar improvements - stage one

--- include/widgets/fxmenubar.h
+++ include/widgets/fxmenubar.h
@@ -1,9 +1,23 @@
 #ifndef __FXMENUBAR_H__
 #define __FXMENUBAR_H__
 
+#include <SupportDefs.h>
 #include <fxdisplay.h>
 #include <fxrect.h>
 #include <fxpoint.h>
+
+#include <vector>
+
+namespace
+{
+    class MenuItems
+    {
+        uint32_t menu_item_i;
+        const char* menu_item_name;
+    };
+
+    std::vector<MenuItems> menus;
+}
 
 namespace FX
 {
@@ -25,10 +39,17 @@ namespace FX
                 display->DrawText("View", menubar_r.x+65, menubar_r.y+15, textColor);
                 display->DrawText("Help", menubar_r.x+95, menubar_r.y+15, textColor);
             }
+            void AddMenu(const char* menu)
+            {
+            }
+            void AddMenuItem(const char* item)
+            {
+            }
             ~FXMenuBar()
             {
             }
         private:
+            uint32_t menu_counter;
     };
 }
 
