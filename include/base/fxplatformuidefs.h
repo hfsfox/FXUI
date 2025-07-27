eb74d37499708f1e3986666915286276deb12c6e
hfsfox <darkkitsunezx128k2309@gmail.com>
2025-07-27 22:27:49 +0300

    FXUI: base: codestyle minor changes, add build option for platforms with touchscreen

--- include/base/fxplatformuidefs.h
+++ include/base/fxplatformuidefs.h
@@ -16,28 +16,38 @@
 #endif
 
 #if defined (PLATFORM_WINDOWS)
-#define BACKEND_WINAPI 1
+    #define BACKEND_WINAPI 1
+    #if defined MULTITOUCH
+        #define HAS_MULTITOUCH
+        //#define HAS_SCREEN_KBD
+    #endif
 #elif defined (PLATFORM_MACOSX)
-#define BACKEND_COCOA 1
+    #define BACKEND_COCOA 1
 #elif defined (PLATFORM_LINUX)
 /*#elif defined (PLATFORM_UNIX_COMPATIBLE) \
 && (!defined(PLATFORM_ANDROID) || !defined(PLATFORM_HAIKU) \
 || !defined(PLATFORM_BEOS) || !defined(PLATFORM_MACOSX) || \
 !defined(PLATFORM_QNX) || !defined(PLATFORM_FUCHSIA) || \
 !defined(PLATFORM_PLAN9))*/
-#define BACKEND_X11 1
+    #define BACKEND_X11 1
+    #if defined MULTITOUCH
+        #define HAS_MULTITOUCH
+    #endif
 #elif defined (PLATFORM_ANDROID)
-#define BACKEND_ANDROID 1
+    #define BACKEND_ANDROID 1
+    #define HAS_MULTITOUCH
+    #define HAS_SCREEN_ROTATE
+    #define HAS_SCREEN_KBD
 #elif defined (PLATFORM_FUCHSIA)
-#define BACKEND_SCENIC 1
+    #define BACKEND_SCENIC 1
 #elif defined (PLATFORM_QNX)
-#define BACKEND_PHOTON 1
+    #define BACKEND_PHOTON 1
 #elif defined (PLATFORM_HAIKU) || defined(PLATFORM_BEOS)
-#define BACKEND_BEAPI 1
+    #define BACKEND_BEAPI 1
 #elif defined (PLATFORM_AMIGA_COMPATIBLE)
-#define BACKEND_MUI 1
+    #define BACKEND_MUI 1
 #elif defined (PLATFORM_RISCOS)
-#define BACKEND_RISCOS 1
+    #define BACKEND_RISCOS 1
 #endif
 
 /*
