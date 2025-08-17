8a2fd17486b586e1265cb4c33cd3270ef20c3574
hfsfox <darkkitsunezx128k2309@gmail.com>
2025-08-17 12:44:07 +0300

    FXUI: base: implement some basic interfaces in cocoa backend

--- src/fxui/platform/macosx/MacOSXAPIWindow.h
+++ src/fxui/platform/macosx/MacOSXAPIWindow.h
@@ -1,18 +1,12 @@
 #include <fxrect.h>
 #ifdef BACKEND_COCOA
 
-#include <Cocoa/Cocoa.h>
-#include <Foundation/Foundation.h>
-#include <Carbon/Carbon.h>
-#include <objc/message.h>
-#include <objc/runtime.h>
-
 //NSApplication       *NSApplicationInstance;
 
-class MacOSXWindow
+class MacOSXAPIWindow
 {
     public:
-        MacOSXWindow(FX::FXRect frame, const char* title);
+        MacOSXAPIWindow(FX::FXRect frame, const char* title);
         virtual bool QuitRequested();
         bool ShouldClose() const { return fShouldClose; }
     public:
@@ -24,6 +18,16 @@ class MacOSXWindow
         void* window = nullptr;
         // CocoaAPIView*
 	    void* view = nullptr;
+};
+
+class MacOSAPIView
+{
+
+};
+
+class MacOSXAPIMenuBar
+{
+
 };
 
 #endif
--- src/fxui/platform/macosx/MacOSXAPIWindow.mm
+++ src/fxui/platform/macosx/MacOSXAPIWindow.mm
@@ -2,12 +2,23 @@
 
 #if defined (BACKEND_COCOA)
 
+#include <Cocoa/Cocoa.h>
+#include <Foundation/Foundation.h>
+#include <Carbon/Carbon.h>
+#include <objc/message.h>
+#include <objc/runtime.h>
+
+#define cls objc_getClass
+#define sel sel_getUid
+#define msg ((id(*)(id, SEL, ...))objc_msgSend)
+#define cls_msg ((id(*)(Class, SEL, ...))objc_msgSend)
+
 @interface CocoaAPIWindow : NSWindow
 {
 }
 @end
 
-@implementation CocoaAPIXWindow
+@implementation CocoaAPIWindow
 
 @end
 
@@ -21,7 +32,13 @@
 
 @end
 
-MacOSXWindow::MacOSXWindow(FX::FXRect frame, const char* title)
+@interface CocoaAPIMenuBar: NSMenu
+@end
+
+@implementation CocoaAPIMenuBar: NSMenu
+@end
+
+MacOSXAPIWindow::MacOSXAPIWindow(FX::FXRect frame, const char* title)
 {
 };
 
