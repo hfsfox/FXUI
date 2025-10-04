fc0c9707fcc6e5a91ccbe674bbcf91c307875e31
hfsfox <darkkitsunezx128k2309@gmail.com>
2025-10-04 07:14:21 +0300

    FXUI: base: add basic description for QNX platform

--- src/fxui/platform/qnx/QNXAPIWindow.cpp
+++ src/fxui/platform/qnx/QNXAPIWindow.cpp
@@ -3,5 +3,6 @@
     #include <screen/screen.h>
     screen_context_t screen_context = 0;
     screen_window_t screen_window = 0;
+    screen_display_t* screen_displays = 0;
     int display_count = 0;
 #endif
