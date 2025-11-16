00d7310fa5e7c0136e45bfcca62ec6789754bfde
hfsfox <darkkitsunezx128k2309@gmail.com>
2025-11-16 14:22:00 +0200

    FXUI: base: some try to add get clock ticks with maximum precise

--- include/base/fxtimeutils.h
+++ include/base/fxtimeutils.h
@@ -12,6 +12,7 @@
 #ifndef     __FXTIMEUTILS__
 #define     __FXTIMEUTILS__
 
+#include <fxplatformdefs.h>
 #include <inttypes.h>
 //#include <unistd.h>
 #include <time.h>
@@ -29,6 +30,27 @@ namespace FX
     {
 
     }*/
+    inline int32_t GetTickCount()
+    {
+        //#ifdef __APPLE__
+        #if defined PLATFORM_MACOSX
+        // could switch to mach_getabsolutetime() maybe
+        struct timeval tm={0,};
+        gettimeofday(&tm,NULL);
+        return (int32_t) (tm.tv_sec*1000 + tm.tv_usec/1000);
+        #elif defined PLATFORM_WINDOWS
+        return (int32_t)(GetTickCount64());
+        #elif defined PLATFORM_LINUX
+        //
+        struct timespec ts={0,};
+        clock_gettime(CLOCK_REALTIME, &ts);
+        return (int32_t) (ts.tv_sec*1000 + ts.tv_nsec/1000000);
+        #else
+        struct timespec ts={0,};
+        clock_gettime(CLOCK_MONOTONIC,&ts);
+        return (int32_t) (ts.tv_sec*1000 + ts.tv_nsec/1000000);
+        #endif
+    }
 }
 
 #endif
