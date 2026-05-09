7e9767649b3b433b690cd6a27dacb6a7d4bb3a3d
hfsfox <darkkitsunezx128k2309@gmail.com>
2026-05-09 12:44:58 +0300

    fxui: all: remove old code version

--- src/fxui/platform/qnx/QNXAPIWindow.cpp
+++
@@ -1,8 +0,0 @@
-#include <fxplatformuidefs.h>
-#ifdef BACKEND_PHOTON
-    #include <screen/screen.h>
-    screen_context_t screen_context = 0;
-    screen_window_t screen_window = 0;
-    screen_display_t* screen_displays = 0;
-    int display_count = 0;
-#endif
